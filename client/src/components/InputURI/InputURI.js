1caa699a1a2cbd56989543625ea0d615d0767b73
Nate <n8blake@mac.com>
2021-07-13 01:14:30 -0400

    Style InputURI

--- client/src/components/InputURI/InputURI.js
+++ client/src/components/InputURI/InputURI.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './InputURI.scss';
 
-function InputURI() {
+function InputURI(props) {
     //do stuff
 
     return (
-        <input type="text" name="inputURI" placeholder="Playlist URI" />
+        <div className="input-wrapper">
+            <input onChange={props.onChange} className=" " type="text" name="inputURI" placeholder="Playlist URI" />
+        </div>
     )
 
 }
