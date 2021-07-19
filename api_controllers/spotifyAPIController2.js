1fadfe156f8a0c53b031dcdcaf8075c6e56edab3
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2021-07-19 16:49:47 -0700

    fixed error and added error controllers

--- api_controllers/spotifyAPIController2.js
+++ api_controllers/spotifyAPIController2.js
@@ -1,5 +1,6 @@
 const SpotifyWebApi = require('spotify-web-api-node')
 const appleMusicAPIController2 = require("./appleMusicAPIController2")
+//const {Playlist} = require("../models")
 
 module.exports = {
     fetchPlaylist: async function (req, res) {
@@ -8,9 +9,22 @@ module.exports = {
         // 3. Create an internal playlist and return
         console.log("::Spotify fetchPlaylist called::")
 
-        const incomingURL = new URL(req.body.playlist_uri)
-        const playlistName = incomingURL.pathname.split('/').pop()
-        console.log("::PlaylistName:: " + playlistName)
+        const workingData = {
+            appleMusicPlaylistURI: '',
+            spotifyPlaylistURI: '',
+            playlistName: '',
+            tracks: []
+        }
+
+        if(req.body.playlist_uri === undefined) {
+            return res.status(400).send({
+                error: "Request body doesn't contain playlist url."
+            })
+        }
+        
+        workingData.spotifyPlaylistURI = new URL(req.body.playlist_uri)
+        const spotifyPlaylistId = workingData.spotifyPlaylistURI.pathname.split('/').pop()        
+        //console.log("::DEBUG:: Spotify playlist name: " + spotifyPlaylistId)
 
         const refresh = process.env.REFRESH_TOKEN
         const spotifyApi = new SpotifyWebApi({
@@ -24,21 +38,41 @@ module.exports = {
         const accessTokenResponse = await spotifyApi.refreshAccessToken()
         spotifyApi.setAccessToken(accessTokenResponse.body.access_token)
 
-        const playlistDetails = await spotifyApi.getPlaylist(playlistName)                
+        const playlistDetails = await spotifyApi.getPlaylist(spotifyPlaylistId)
 
-        const tracksResponse = await spotifyApi.getPlaylistTracks(playlistName)
-        const trackISRCs = tracksResponse.body.items.map(item => {
-            return item.track.external_ids.isrc
-        })
+        // TODO: handle error
+        //       It is possible that no playlist is found. 
+        workingData.playlistName = playlistDetails.body.name        
 
-        const playlistData = {
-            'Name': playlistDetails.body.name,
-            'TracksByISRC': trackISRCs
+        const trackDetails = await spotifyApi.getPlaylistTracks(spotifyPlaylistId)
+        for(let i=0; i<trackDetails.body.items.length; i++ ){
+            const currentItem = trackDetails.body.items[i]
+            const currentTrackDetail = {
+                artists: currentItem.track.artists.map(item =>{
+                    return item.name
+                }),
+                trackName: currentItem.track.name,
+                releaseDate: currentItem.track.album.release_date,
+                album: currentItem.track.album.name,
+                images: currentItem.track.album.images,
+                appleTrackId: '',
+                spotifyTrackId: currentItem.track.id,
+                isrc: currentItem.track.external_ids.isrc,                
+                errorMatch: false
+            }            
+
+            workingData.tracks.push(currentTrackDetail)
         }
+        
+        console.log("::DEBUG:: WorkingData")
+        console.log(workingData)                
 
-        console.log(playlistData)
+        const createResponse = await appleMusicAPIController2.createPlaylist(workingData)
+        const applePlaylistUrl = `https://music.apple.com/library/playlist/${createResponse.data.data[0].id}`
+        console.log("::Playlist url:: " + applePlaylistUrl.toString());
 
-        const createResponse = await appleMusicAPIController2.createPlaylist(playlistData)
+        workingData.appleMusicPlaylistURI = applePlaylistUrl
+
         return res.status(createResponse.status).send(createResponse.data.data)
     }
 }
