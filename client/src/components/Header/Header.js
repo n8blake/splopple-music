011f71b5f16a25b15cecc8f7c5ba00676ca9bc06
Nate <n8blake@mac.com>
2021-07-19 12:03:48 -0400

    Add animation to logo component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
+import { useStoreContext } from "../../utils/GlobalState";
 import Logo from '../logo/logo';
 import './Header.scss';
 
 export default function Header() {
 
+    const [showSpinner, setShowSpinner] = useState(false);
+    const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+      if(state.loading){
+        setShowSpinner(true);
+      } else {
+        setShowSpinner(false);
+      }
+    }, [state.loading])
+
     const showNav = false;
+
+
 
     return (
         <div className='text-center'>            
-            <Logo />
-            <div>
-              <h1>Splopple Music</h1>
-              <p>Giving the people what they want: Spotify and Apple Music Harmony.</p>
+            <div className="d-flex justify-content-center">
+              <Logo spin={showSpinner} />
             </div>
+            {
+              ((state.inputListResults && state.inputListResults.tracks) || state.loading) ? (
+                <div>
+                  
+                </div>
+              ) : (
+                <div>
+                  <h1>Splopple Music</h1>
+                  <p>Giving the people what they want: Spotify and Apple Music Harmony.</p>
+                </div>
+              )
+            }
+            {
+              showSpinner ? (
+                <div>Loading...</div>
+              ) : (
+                <div></div>
+              )
+            }
             { showNav ? (
               <nav>
                 <ul className="nav justify-content-center">
--- client/src/components/logo/logo.js
+++ client/src/components/logo/logo.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogoSVG from './logo.svg';
+import LogoBG from './logo-bg.svg';
+import musicNote from './music-note-white.svg';
+import soundLines from './sound-lines-white.svg';
 import './logo.scss';
 
-function Logo() {
-    //do logo stuff
+function Logo(props) {
+
+    useEffect(() => {
+
+    }, [props.spin])
 
     return (
-        <img className="logo-default" alt="logo" src={LogoSVG}/>
+        props.spin ? (
+            <div className="logo-spinning-container">
+                <img className="logo-spinning-bg" alt="logo" src={LogoBG}/>
+                <div className="music-note-container">
+                    <img className="music-note-spinning" alt="music note" src={musicNote} />
+                </div>
+                <div className="sound-lines-container">
+                    <img className="sound-lines-spinning" alt="sound lines" src={soundLines} />
+                </div>
+            </div>
+        ) : (
+            <img className="logo-default" alt="logo" src={LogoSVG}/>
+        )
     )
 
 }
